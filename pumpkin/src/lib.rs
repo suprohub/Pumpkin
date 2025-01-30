2912618ee0cc4e40056f9d7f08af49339877c7f0
suprohub <suprohub@gmail.com>
2025-01-30 16:31:21 +0400

    Add one useful function

--- pumpkin/src/lib.rs
+++ pumpkin/src/lib.rs
@@ -46,10 +46,16 @@ pub static LOGGER_IMPL: LazyLock<Option<(Box<dyn Log>, LevelFilter)>> = LazyLock
             logger = logger.without_timestamps();
         }
 
-        Some((Box::new(logger.with_level(LevelFilter::Info)
-            .with_colors(ADVANCED_CONFIG.logging.color)
-            .with_threads(ADVANCED_CONFIG.logging.threads)
-            .env()), LevelFilter::Info))
+        Some((
+            Box::new(
+                logger
+                    .with_level(LevelFilter::Info)
+                    .with_colors(ADVANCED_CONFIG.logging.color)
+                    .with_threads(ADVANCED_CONFIG.logging.threads)
+                    .env(),
+            ),
+            LevelFilter::Info,
+        ))
     } else {
         None
     }
